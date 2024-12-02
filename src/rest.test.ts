0cfa5711327d65d453498a5d611c0d8b30177f9d
Flavian <flavian.leprettre@ito.am>
2024-12-02 16:44:44 +0100

    refactor: update test setup and move get user test to rest test file

---
+++ src/rest.test.ts
@@ -0,0 +1,42 @@
+import { beforeEach, describe, expect, test } from 'vitest'
+
+import type { RestClient } from './rest.js'
+
+import { createRestClient } from './rest.js'
+
+const getEnvOrThrow = (key: string) => {
+  const value = process.env[key]
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not set`)
+  }
+  return value
+}
+
+type RestContext = {
+  authClient: RestClient
+  client: RestClient
+}
+
+beforeEach<RestContext>((ctx) => {
+  ctx.authClient = createRestClient({
+    key: getEnvOrThrow('LNM_API_KEY'),
+    passphrase: getEnvOrThrow('LNM_API_PASSPHRASE'),
+    secret: getEnvOrThrow('LNM_API_SECRET'),
+  })
+
+  ctx.client = createRestClient({
+    key: '',
+    passphrase: '',
+    secret: '',
+  })
+})
+
+describe('get user route', () => {
+  test<RestContext>('should throw if not authenticated', async (ctx) => {
+    await expect(ctx.client.user.get()).rejects.toThrow()
+  })
+
+  test<RestContext>('should return the user', async (ctx) => {
+    await expect(ctx.authClient.user.get()).resolves.toBeDefined()
+  })
+})
