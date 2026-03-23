9afa487747585ad5abbb75f584bb44cc4a27d291
Victor Afanassieff <victor@afanassieff.com>
2026-03-23 09:59:53 +0100

    chore: cleaning

--- knip.ts
+++ knip.ts
@@ -1,8 +1,6 @@
-// oxlint-disable-next-line no-self-import -- We are safe bro
 import type { KnipConfig } from 'knip'
 
 export default {
-  ignore: ['./examples/**'],
   oxlint: { config: ['.oxlintrc.json'] },
   typescript: { config: ['tsconfig.json', 'tsconfig.build.json'] },
   entry: ['./src/v3/index.ts', './src/v2/index.ts'],
